450daeda227957b4328e8778536a2baf5f2b4601
OraonShivam21 <oraonshivam.0302@gmail.com>
2026-08-11 15:45:24 +0530

    Given an integer N, return the number of digits in N.

---
+++ questions/Number of digits in N/code.js
@@ -0,0 +1,20 @@
+/**
+ * QUESTION: Given an integer N, return the number of digits in N.
+ */
+
+const fs = require("fs");
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/);
+let original = input[0];
+let number = original;
+
+if (number < 0) number = -number;
+
+let count = 1;
+while (number > 0) {
+  number = parseInt(number / 10);
+
+  if (number !== 0) count++;
+}
+
+console.log("The number of digits in", original, "are:", count);
